17ba58dcd890b53f7074cddccadf0acee9f3a986
Enzo <potc.zone+git@gmail.com>
2015-05-05 19:37:24 +0800

    working translation for Work objects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,6 @@ from portfolio import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 	#url(r'^(?P<pk>[0-9]+)/', views.DetailView.as_view(), name='detail')
-	url(r'lang/(?P<language>\D+)/', views.languageSelect, name='language'),
+	url(r'lang/(?P<lang_code>\D+)/', views.languageSelect, name='language'),
 	#url(r'lang/fr/', views.languageSelect, name='language'),
 )
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from django.utils.translation import (
+	LANGUAGE_SESSION_KEY, check_for_language, get_language, to_locale,
+)
+
+from portfolio.models import Work
+
+
+
+
 
 def index(request):
 	
+	works = Work.objects.all()
 	
+	w = works[0]
+	title = w.title
 	
 	context = {
+		'works' : works,
+		'wtitle' : title,
 	}
 	
 	return render(request, 'portfolio/index.html', context)
 	
 	
-def languageSelect(request, language):
-	return HttpResponse('coucou ' +language)
+def languageSelect(request, lang_code):
+	#if language == 'en' or language == 'fr' or language == 'zh':
+	#	request.session['django_language'] = language
+		
+	response = HttpResponseRedirect('/')
+	
+	if lang_code and check_for_language(lang_code):
+		if hasattr(request, 'session'):
+			request.session[LANGUAGE_SESSION_KEY] = lang_code
+		else:
+			response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code,
+								max_age=settings.LANGUAGE_COOKIE_AGE,
+								path=settings.LANGUAGE_COOKIE_PATH,
+								domain=settings.LANGUAGE_COOKIE_DOMAIN)
+		
+	#return response
+	return HttpResponse('coucou ' +lang_code+ ' '+request.session[LANGUAGE_SESSION_KEY])
