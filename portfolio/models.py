fdd8e8f062e6607e44bef3e22eac9c06647444ce
Enzo <potc.zone+git@gmail.com>
2015-05-08 03:28:04 +0800

    Use pillow Image to create greyscale icons in Tech Object

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,22 +1,89 @@
 from django.db import models
 from django.utils.html import format_html
 from django.conf import settings
+from PIL import Image
+
+import os
+from cStringIO import StringIO
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 """
 Tech : ex python
 """
 class Tech(models.Model):
 	name = models.CharField(max_length=20)
-	icon = models.ImageField(upload_to = 'images/icons/')
+	icon = models.ImageField(upload_to = 'images/icons/', db_column="icon")
+	icon_g = models.ImageField(upload_to = 'images/icons/', blank=True)
 	notes = models.CharField(max_length=200, blank=True)
 	
 	def __unicode__(self):
 		return self.name
 		
 	def icon_32(self):
-		return format_html('<img style="height:32px; width:32px" src="media/{}"/>',
-						   self.icon,
-						)
+		return format_html('<img style="height:32px; width:32px" src="'+settings.MEDIA_URL+'{}"/>', self.icon,)
+						
+	def __init__(self, *args, **kwargs):
+		super(Tech, self).__init__(*args, **kwargs)
+		self.__original_icon = self.icon
+		
+		
+	'''
+	#renamed icon to _icon
+	@property
+	def icon(self):
+		return self._icon
+		
+	@icon.setter
+	def icon(self, value):
+		if(self._icon != value):
+			self._image_changed = True
+			self._debug_val = "self._icon : "+ self._icon +" AND value : "+ value
+		#self.icon_g = SimpleUploadedFile(path_g, image_grey)
+		self._icon = value
+	'''
+	#overide save
+	def save(self, *args, **kwargs):
+		'''
+		if getattr(self, '_image_changed', True):
+			self.icon_g = getattr(self, '_debug_val')
+			self._image_changed = False
+		'''
+		if self.__original_icon != self.icon:
+			#create the greyscale icon
+			
+			#open the normal image
+			image = Image.open(self.icon)
+			
+			#convert to greyscale
+			image = image.convert('LA')
+			
+			# save the thumbnail to memory
+			temp_handle = StringIO()
+			image.save(temp_handle, 'png')
+			temp_handle.seek(0) # rewind the file
+			
+			# save to the thumbnail field
+			base = os.path.split( self.icon.name )[-1]
+			suf = SimpleUploadedFile(os.path.splitext(base)[0], #without extension
+									 temp_handle.read(),
+									 content_type='image/png')
+			self.icon_g.save(suf.name+'g.png', suf, save=False)
+			
+			'''
+			outfile = os.path.splitext(infile)[0] + ".thumbnail"
+			
+			try:
+				im = Image.open(infile)
+				im.thumbnail(size)
+				im.save(outfile, "PNG")
+			except IOError:
+				print("cannot create thumbnail for", infile)
+			'''
+			
+			
+			
+		super(Tech, self).save(*args, **kwargs)
+		
 	
 class Tag(models.Model):
 	name = models.CharField(max_length=200)
