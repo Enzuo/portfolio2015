0bd359a055e116e923baea789c4e7cb99317c626
Enzo <potc.zone+git@gmail.com>
2015-05-14 23:58:52 +0800

    Getting ready for deployement on Heroku, except for media files

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio.settings")
+#os.environ["DJANGO_SETTINGS_MODULE"] =  "myportfolio.settings"
+
+#application = Cling(get_wsgi_application())
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
