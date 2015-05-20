1f8a5c3123df7a738943710cb03719ff6806674b
Enzo <potc.zone+git@gmail.com>
2015-05-21 06:24:18 +0800

    Debug false ready for push to heroku

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -3,20 +3,31 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'myportfolio.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    
-    url(r'^admin/', include(admin.site.urls)),  
-    url(r'^redactor/', include('redactor.urls')),
-    #url(r'^$', include('portfolio.urls')),
-    url(r'^', include('portfolio.urls')),
+	# Examples:
+	# url(r'^$', 'myportfolio.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	
+	url(r'^admin/', include(admin.site.urls)),  
+	url(r'^redactor/', include('redactor.urls')),
+	#url(r'^$', include('portfolio.urls')),
+	url(r'^', include('portfolio.urls')),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'media/(?P<path>.*)',
-        'serve',
-        {'document_root': settings.MEDIA_ROOT}), )
+	urlpatterns += patterns(
+		'django.views.static',
+		(r'media/(?P<path>.*)',
+		'serve',
+		{'document_root': settings.MEDIA_ROOT}), )
+	"""
+	#to test pages 404 and 500 when debugging
+	urlpatterns += patterns('',
+		(r'^404/',
+			'django.views.generic.simple.' \
+			'direct_to_template',
+			{'template': '404.html'}),
+		(r'^500/',
+			'django.views.generic.simple.' \
+			'direct_to_template',
+			{'template': '500.html'}))"""
