54b005135b19b16173440b5b661f807beca37d2b
Enzo <potc.zone+git@gmail.com>
2015-05-21 05:01:11 +0800

    Translations

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy
 
 class ContactForm(forms.Form):
-	email = forms.EmailField(label= ugettext_lazy('Email'), required=True)
-	name = forms.CharField(label= ugettext_lazy('Name'), max_length=100, required=True)
+	email = forms.EmailField(label= ugettext_lazy('Your email'), required=True)
+	name = forms.CharField(label= ugettext_lazy('Your name'), max_length=100, required=True)
 	subject = forms.CharField(label= ugettext_lazy('Subject'), max_length=100, required=True)
 	message = forms.CharField(label= ugettext_lazy('Message'), widget=forms.Textarea)
